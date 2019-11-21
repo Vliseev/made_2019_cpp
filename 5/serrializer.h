2ec1d04aac60803257dbe285d30254beac58fb25
Vliseev <Vliseev.92@mail.ru>
2019-11-21 17:17:57 +0300

    fix conv

--- 5/serrializer.h
+++ 5/serrializer.h
@@ -4,7 +4,6 @@
 // serializer.h
 #pragma once
 #include <charconv>
-#define CHECK_TYPE(x) std::is_same<x, uint64_t>() || std::is_same<x, bool>()
 
 enum class Error { NO_ERROR, CORRUPTED_ARCHIVE };
 
@@ -12,11 +11,12 @@ class Serializer {
     static constexpr char Separator = ' ';
     std::ostream& out_;
 
- public:
-    explicit Serializer(std::ostream& out) : out_(out) {}
+   public:
+    explicit Serializer(std::ostream& out) : out_(out) {
+    }
 
     template <class T>
-    Error Save(T& object) {
+    Error Save(T object) {
         return object.serialize(*this);
     }
 
@@ -25,13 +25,10 @@ class Serializer {
         return Process(args...);
     }
 
- private:
+   private:
     // Process использует variadic templates
     template <class T>
-    Error Process(T& val) {
-        typedef typename std::remove_reference<decltype(val)>::type C;
-        static_assert(std::is_same<C, bool>() || std::is_same<C, uint64_t>());
-
+    Error Process(T val) {
         SaveVal(val);
         if (out_)
             return Error::NO_ERROR;
@@ -40,10 +37,7 @@ class Serializer {
     }
 
     template <class T, class... ArgsT>
-    Error Process(T& val, ArgsT&... args) {
-        typedef typename std::remove_reference<decltype(val)>::type C;
-        static_assert(std::is_same<C, bool>() || std::is_same<C, uint64_t>());
-
+    Error Process(T val, ArgsT... args) {
         SaveVal(val);
         out_ << Separator;
         if (out_)
@@ -52,11 +46,11 @@ class Serializer {
             return Error::CORRUPTED_ARCHIVE;
     }
 
-    void SaveVal(uint64_t& value) {
+    void SaveVal(uint64_t value) {
         out_ << value;
     }
 
-    void SaveVal(bool& value) {
+    void SaveVal(bool value) {
         if (value)
             out_ << "true";
         else
@@ -68,8 +62,9 @@ class Deserializer {
     static constexpr char Separator = ' ';
     std::istream& in_;
 
- public:
-    explicit Deserializer(std::istream& in) : in_(in) {}
+   public:
+    explicit Deserializer(std::istream& in) : in_(in) {
+    }
 
     template <class T>
     Error Load(T& object) {
@@ -81,16 +76,11 @@ class Deserializer {
         return Process(args...);
     }
 
- private:
+   private:
     template <class T>
     Error Process(T& val) {
-        typedef typename std::remove_reference<decltype(val)>::type C;
-        if (std::is_same<C, uint64_t>() || std::is_same<C, bool>()) {
-            if (LoadVal(val) == Error::NO_ERROR) {
-                return Error::NO_ERROR;
-            } else {
-                return Error::CORRUPTED_ARCHIVE;
-            }
+        if (LoadVal(val) == Error::NO_ERROR) {
+            return Error::NO_ERROR;
         } else {
             return Error::CORRUPTED_ARCHIVE;
         }
@@ -98,13 +88,8 @@ class Deserializer {
 
     template <class T, class... ArgsT>
     Error Process(T& val, ArgsT&... args) {
-        typedef typename std::remove_reference<decltype(val)>::type C;
-        if (std::is_same<C, uint64_t>() || std::is_same<C, bool>()) {
-            if (LoadVal(val) == Error::NO_ERROR) {
-                return Process(args...);
-            } else {
-                return Error::CORRUPTED_ARCHIVE;
-            }
+        if (LoadVal(val) == Error::NO_ERROR) {
+            return Process(args...);
         } else {
             return Error::CORRUPTED_ARCHIVE;
         }
